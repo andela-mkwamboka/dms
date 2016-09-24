c518206bc466491a994d27afb1937f8f000a4c4a
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-09-24 16:44:23 +0300

    Set-up test

---
+++ test/server/test.js
@@ -0,0 +1,10 @@
+// Dependencies
+const expect = require('chai').expect;
+// const chaiHttp = require('chai-http');
+
+describe(('TEST'), () => {
+  it('hey', done => {
+    expect('hey').to.be.equal('hey');
+    done();
+  });
+});
