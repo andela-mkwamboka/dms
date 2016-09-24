f96518b61d74d9e68cfeafe214f48e7507a14f02
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-09-24 16:06:21 +0300

    Mongodb config using mongoose

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+// Dependancies
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// DB connection
+require('./server/config/db');
+
 const app = express();
 
+// Configure express to use bodyParser() to get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Start server
 app.listen(3000, () => {
   console.log('Running');
 });
