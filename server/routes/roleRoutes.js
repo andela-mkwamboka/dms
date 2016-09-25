40320b2c9b4d5f0ef177b5d83336635818441b3d
andela-mkwamboka <monicah.kwamboka@andela.com>
2016-09-26 00:32:37 +0300

    Roles functions

--- server/routes/roleRoutes.js
+++ server/routes/roleRoutes.js
@@ -1,4 +1,23 @@
 // Dependencies
 const router = require('express').Router();
 
+// Controllers
+const roles = require('../controllers/rolesCtrl');
+
+// Middleware
+const middleware = require('../middleware/middleware');
+
+// Token authentication
+router.use(middleware.authenticate);
+
+// DOCUMENT ENDPOINTS
+router.route('/')
+  .post(roles.create) // Creates a new role.
+  .get(roles.getAll); // Get all roles.
+
+router.route('/:role_id')
+  .get(roles.getRole) // Find role.
+  .delete(roles.delete); // Delete role.
+
+
 module.exports = router;
--- test/server/role.spec.js
+++ test/server/role.spec.js
@@ -1,104 +1,94 @@
-// // Dependencies
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const api = require('./../../server');
-// const mongoose = require('mongoose');
-//
-// const Role = mongoose.model('Role');
-// const expect = chai.expect;
-//
-// chai.use(chaiHttp);
-//
-// describe('ROLE', () => {
-//   let token;
-//   let roleID;
-//   const admin = {
-//     title: 'admin',
-//   };
-//   const user = {
-//     username: 'Malik',
-//     first: 'Malik',
-//     last: 'Kinaro',
-//     email: 'mk@gmail.com',
-//     password: '1234',
-//     role: 'admin',
-//   };
-//   Role.collection.drop();
-//   before((done) => {
-//     chai.request(api)
-//     .post('/users')
-//     .send(user)
-//     .end((err, res) => {
-//       token = res.body.token;
-//       done();
-//     });
-//   });
-//   describe('POST', () => {
-//     it('Should create a new role', (done) => {
-//       chai.request(api)
-//       .post('/roles')
-//       .send(admin)
-//       .set({ Authorization: 'Bearer ' + token })
-//       .end((err, res) => {
-//         expect(res.status).to.be.equal(200);
-//         expect(res.body).to.be.a('object');
-//         expect(res.body).to.have.all.keys('message', 'role');
-//         expect(res.body.role).to.have.keys('title', '_id', '__v');
-//         done();
-//       });
-//     });
-//
-//     it('New role should be unique', (done) => {
-//       chai.request(api)
-//       .post('/roles')
-//       .send(admin)
-//       .set({ Authorization: 'Bearer ' + token })
-//       .end((err, res) => {
-//         expect(res.status).to.be.equal(409);
-//         expect(res.body).to.be.a('object');
-//         expect(res.body).to.have.key('message');
-//         expect(res.body.message).to.be.equal('Title already exists');
-//         done();
-//       });
-//     });
-//   });
-//   describe('GET', () => {
-//     it('/roles/: Returns all roles', (done) => {
-//       chai.request(api)
-//         .get('/roles')
-//         .set({ Authorization: 'Bearer ' + token })
-//         .end((err, res) => {
-//           roleID = res.body.roles[0]._id;
-//           expect(res.status).to.equal(200);
-//           expect(res.body.roles).to.be.a('Array');
-//           expect(res.body.roles.length).to.equal(1);
-//           expect(res.body.roles[0]).to.have.all.keys('title', '_id');
-//           done();
-//         });
-//     });
-//
-//     it('/roles/<id>: Find role by id.', (done) => {
-//       chai.request(api)
-//         .get('/roles/' + roleID)
-//         .set({ Authorization: 'Bearer ' + token })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           expect(res.body).to.be.a('object');
-//           expect(res.body).to.have.all.keys('_id', 'title');
-//           done();
-//         });
-//     });
-//   });
-//   describe('DELETE', () => {
-//     it('/roles/<id>: Delete role.', (done) => {
-//       chai.request(api)
-//         .delete('/roles/' + roleID)
-//         .set({ Authorization: 'Bearer ' + token })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(202);
-//           expect(res.body.message).to.equal('Successfully deleted');
-//           done();
-//         });
-//     });
-//   });
-// });
+// Dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const api = require('./../../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('ROLE', () => {
+  const admin = {
+    title: 'user',
+  };
+  let token;
+  before((done) => {
+    chai.request(api)
+    .post('/users/login')
+    .send({
+      username: 'john',
+      password: '1234',
+    })
+    .end((err, res) => {
+      token = res.body.token;
+      done();
+    });
+  });
+  describe('POST', () => {
+    it('Should create a new role', (done) => {
+      chai.request(api)
+      .post('/roles')
+      .send(admin)
+      .set({ Authorization: 'Bearer ' + token })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.all.keys('message', 'role');
+        expect(res.body.role).to.have.keys('title', '_id', '__v');
+        done();
+      });
+    });
+
+    it('New role should be unique', (done) => {
+      chai.request(api)
+      .post('/roles')
+      .send(admin)
+      .set({ Authorization: 'Bearer ' + token })
+      .end((err, res) => {
+        expect(res.status).to.be.equal(409);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.key('message');
+        expect(res.body.message).to.be.equal('Title already exists');
+        done();
+      });
+    });
+  });
+  describe('GET', () => {
+    it('/roles/: Returns all roles', (done) => {
+      chai.request(api)
+        .get('/roles')
+        .set({ Authorization: 'Bearer ' + token })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.roles).to.be.a('Array');
+          expect(res.body.roles.length).to.equal(2);
+          expect(res.body.roles[0]).to.have.all.keys('title', '_id');
+          done();
+        });
+    });
+
+    it('/roles/<id>: Find role by id.', (done) => {
+      chai.request(api)
+        .get('/roles/57d11f35b0a303c1186270ad')
+        .set({ Authorization: 'Bearer ' + token })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.a('object');
+          expect(res.body).to.have.all.keys('_id', 'title');
+          done();
+        });
+    });
+  });
+  describe('DELETE', () => {
+    it('/roles/<id>: Delete role.', (done) => {
+      chai.request(api)
+        .delete('/roles/57d11f35b0a303c1186270ad')
+        .set({ Authorization: 'Bearer ' + token })
+        .end((err, res) => {
+          expect(res.status).to.equal(202);
+          expect(res.body.message).to.equal('Successfully deleted');
+          done();
+        });
+    });
+  });
+});
